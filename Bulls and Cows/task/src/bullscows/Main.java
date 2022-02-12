bce1ec062309eec4138ccca3c270ec7def82bc04
Matt Villa <matthewvilla805@gmail.com>
2022-02-11 20:08:50 -0800

    random input finished 3/7

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,18 +1,45 @@
 package bullscows;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        Grader grader = new Grader();
+        //Grader grader = new Grader();
         Scanner scanner = new Scanner(System.in);
 
-        StringBuilder input = new StringBuilder();
-        input.append(scanner.nextLine());
+        //StringBuilder input = new StringBuilder();
+        //input.append(scanner.nextLine());
 
-        StringBuilder secretCode = new StringBuilder("9305");
-        grader.gradeCode(secretCode, input);
+        //StringBuilder secretCode = new StringBuilder("9305");
+        //grader.gradeCode(secretCode, input);
+        int inputLength = scanner.nextInt();
+        randomGenerator(inputLength);
 
+    }
+
+    public static void randomGenerator(int length) {
+
+        if (length > 10){
+            System.out.println("Error: can't generate a secret number with a " +
+                    "length of 11 because there aren't enough unique digits");
+            return;
+        }
+
+        StringBuilder secretCode = new StringBuilder();
+
+        List<Integer> randomList = new ArrayList<>(List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+        while (randomList.get(0) == 0) {
+            Collections.shuffle(randomList);
+        }
+
+        for (int i = 0; i < length; i++){
+            secretCode.append(randomList.get(i));
+        }
+
+        System.out.printf("The random secret number is %s", secretCode);
     }
 }
